62436bc915893966326e11c2ba3dfa62d954765d
Rajmund <rajmund.matus@cmo24.com>
2020-07-27 16:40:20 +0200

    until backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import { data } from "./data";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+
 function App() {
   const openMenu = () => {
     document.querySelector(".sidebar").classList.add("open");
@@ -11,97 +17,39 @@ function App() {
   };
 
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>&#9776;</button>
-          <a href="./index.html">boltom</a>
-        </div>
-        <div className="header-links">
-          <a href="./cart.html">Cart</a>
-          <a href="./signin.html">Sign In</a>
-        </div>
-      </header>
-      <aside className="sidebar">
-        <h3>Shopping categories</h3>
-        <span>
-          <button className="sidebar-close-button" onClick={closeMenu}>
-            {"<"}
-          </button>
-        </span>
-        <ul>
-          <li>Pants</li>
-          <li>Shirts</li>
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="./product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="price">$60</div>
-                <div className="rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="./product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="price">$60</div>
-                <div className="rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="./product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="price">$60</div>
-                <div className="rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img
-                  className="product-image"
-                  src="/images/d1.jpg"
-                  alt="product"
-                />
-                <div className="product-name">
-                  <a href="./product.html">Slim shirt</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="price">$60</div>
-                <div className="rating">4.5 stars (10 reviews)</div>
-              </div>
-            </li>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}>&#9776;</button>
+            <Link to="/">boltom</Link>
+          </div>
+          <div className="header-links">
+            <a href="./cart.html">Cart</a>
+            <a href="./signin.html">Sign In</a>
+          </div>
+        </header>
+        <aside className="sidebar">
+          <h3>Shopping categories</h3>
+          <span>
+            <button className="sidebar-close-button" onClick={closeMenu}>
+              {"<"}
+            </button>
+          </span>
+          <ul>
+            <li>Pants</li>
+            <li>Shirts</li>
           </ul>
-        </div>
-      </main>
-      <footer className="footer">All rights reserved ©</footer>
-    </div>
+        </aside>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" exact component={ProductScreen} />
+            <Route path="/" exact component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All rights reserved ©</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
